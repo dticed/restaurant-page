5b28be87dedede4150d499c5060ca9679deba173
dticed <lucaspierrealencar@gmail.com>
2021-07-30 11:44:14 -0300

    trying to fix css grid

--- src/menu.js
+++ src/menu.js
@@ -1,20 +1,33 @@
 function createMenu() {
     const menu = document.createElement('div');
     menu.classList.add('menu')
-    menu.appendChild(createImg());
-    menu.appendChild(createImg());
-    menu.appendChild(createImg());
-    menu.appendChild(createImg());
+    menu.appendChild(createImg('pizza1', 'pizza', 'Margherita/Gorgonzola'));
+    menu.appendChild(createImg('pizza2', 'pizza', 'Margherita/Bacon'));
+    menu.appendChild(createImg('pao1', 'sourdough bread', 'Sourdough bread'));
+    menu.appendChild(createImg('pao2', 'sourdough bread', 'Sourdough bread'));
     return menu;
 }
 
-function createImg() {
-    const item = document.createElement('img');
-    item.classList.add('teste')
-    item.src = "../dist/img/pizza1.jpg"
-    item.alt = "pizza"
+function createImg(title, alt, text) {
+    const itemBox = document.createElement('div');
+    const itemImage = document.createElement('img');
+    itemBox.classList.add('menu-item');
+    itemImage.src = `../dist/img/${title}.jpg`
+    itemImage.alt = alt
 
-    return item;
+    
+
+    itemBox.append(itemImage);
+    itemBox.append(createParagraph(text));
+
+    return itemBox;
+}
+
+function createParagraph(text) {
+    const paragraph = document.createElement('p');
+    paragraph.textContent = text;
+
+    return paragraph;
 }
 
 function loadMenu() {
